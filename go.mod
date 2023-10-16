2fb4cd199cdc52d701821400628fdca41022156a
Paul Ohrt <paul.ohrt@internetstiftelsen.se>
2023-10-16 19:28:11 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zaouldyeck/identify
+
+go 1.21.2
+
+require github.com/google/gopacket v1.1.19 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"log"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	for _, device := range devices {
+		fmt.Println(device.Name)
+		for _, address := range device.Addresses {
+			fmt.Printf("      IP:      %s\n", address.IP)
+			fmt.Printf("      Netmask: %s\n", address.Netmask)
+		}
+	}
+}
